b894113d1cc830693e1891527efc2cf3542658d0
jsubbie <subbie.jason@gmail.com>
2017-12-08 22:33:43 -0500

    update

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,6 +1,5 @@
 import os 
 import csv 
-import datetime
 
 pyboss_csv = os.path.join("employee_data1.csv")
 
@@ -26,17 +25,27 @@ with open(pyboss_csv, newline="") as csvfile:
                 first_Name.append(name1[0])
                 last_Name.append(name1[1])
                 name1 = []
-# DOB        
-        dob.append(row[2])
-# social security conversion                
-        Social.append(row[3])
-        split_ssn = list(row["Social"])
-        split_ssn[0:3] = ("*", "*", "*")
-        split_ssn[4:6] = ("*", "*")
-        joined_ssn = "".join(split_ssn)
-        State.append(row[4])
+        # dob.append(row[2])
+        # for j in range(len(dob)):
+        #         date_conversion = [j]
+        #         date_conversion.appendstrftime('%m/%d/%Y')  
+        #         date_conversion = []
+        # Social.append(row[3])
+        # State.append(row[4])
 # date_reformat = range(.appendstrftime('%m/%d/%Y'))
-        date1 = datetime.datetime.strptime(dob, "%Y-%m-%d")
-        date_conversion = date1.strftime("%m/%d/%Y")
   
-print(first_Name, last_Name, date_conversion) 
+print(first_Name, last_Name) 
+
+# DOB        
+#         dob.append(row[2])
+# # social security conversion                
+#         Social.append(row[3])
+#         split_ssn = list(row["Social"])
+#         split_ssn[0:3] = ("*", "*", "*")
+#         split_ssn[4:6] = ("*", "*")
+#         joined_ssn = "".join(split_ssn)
+#         State.append(row[4])
+# # date_reformat = range(.appendstrftime('%m/%d/%Y'))
+#         date1 = datetime.datetime.strptime(dob, "%Y-%m-%d")
+#         date_conversion = date1.strftime("%m/%d/%Y")
+  
